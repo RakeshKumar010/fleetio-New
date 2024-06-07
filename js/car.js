1155740a0983d6061bfedd685442d6e950087379
rakesh <rakeshkumar748844@gmail.com>
2024-06-07 13:06:48 +0530

    add-some-page

--- js/car.js
+++ js/car.js
@@ -55,3 +55,41 @@ tabToChangeCar[4].addEventListener('click',()=>{
 
 })
  
+
+// closure
+const rightIcon = document.querySelector('#rightIcon');
+const leftIcon = document.querySelector('#leftIcon');
+const carHeroImgCh = document.querySelector('#carHeroImgCh');
+const carClosureContainer = document.querySelector('#carClosureContainer');
+const carClosureItem = document.querySelectorAll('.car-closure-item');
+
+// Total number of items
+let totalItem = carClosureItem.length;
+// Current index
+let currentIndex = 0;
+
+// Width of each item - assuming each item is 255px wide
+const itemWidth = 255;
+
+// Event listener for the right icon
+rightIcon.addEventListener('click', () => {
+  // Increment currentIndex or reset if at the last item
+  currentIndex = (currentIndex + 1) % totalItem;
+  carClosureContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+});
+
+// Event listener for the left icon
+leftIcon.addEventListener('click', () => {
+  // Decrement currentIndex or set to the last item if at the first item
+  currentIndex = (currentIndex - 1 + totalItem) % totalItem;
+  carClosureContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+});
+
+
+carClosureItem.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    carHeroImgCh.src = carClosureItem[index].src;
+  });
+});
+
+
